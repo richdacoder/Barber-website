5ecc663cf6c98ea3088f069d4c548791da1e5d47
richard williams <richard.williams5697@yahoo.com>
2025-09-15 14:52:51 +0900

    creaated queries page with codeaa

---
+++ models/queries.js
@@ -0,0 +1,15 @@
+const getStudents = (request, response) => {
+    var responseReturn = new ResponseClass();
+    pool.query('SELECT * FROM students ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+
+        responseReturn.status = true;
+        responseReturn.code = 200;
+        responseReturn.message = "Success";
+        responseReturn.data = results.rows;
+
+        response.status(200).json(responseReturn);
+    })
+}
