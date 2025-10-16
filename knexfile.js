caa1e204433b13424892b9fc9f1f9425298b924e
richard williams <richard.williams5697@yahoo.com>
2025-10-16 16:14:23 -0400

    made migration file for add service column to appointments

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+    },
+    migrations: {
+      directory: './migrations', // ✅ tells Knex where to put migration files
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+};
