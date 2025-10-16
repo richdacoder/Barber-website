1455fae1e643225c557e367c1fbb9c9fba20f9ab
richard williams <richard.williams5697@yahoo.com>
2025-10-16 17:03:17 -0400

    added service to appointment table

--- migrations/20251016201057_add_service_to_appointments.js
+++ migrations/20251016201057_add_service_to_appointments.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  console.log("🚀 Running migration: add_service_to_appointments");
+  await knex.schema.table('appointments', function(table) {
+    table.string('service');
+  });
 };
-
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+console.log("remove 'Service' column...");
+await knex.schema.table("appointments", function (table){
+  table.string("service");
+});
+
 };
